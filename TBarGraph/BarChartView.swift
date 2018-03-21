3ffa6cf29739f440d4c9c8b3fa2019de7edc2847
Teja <chundruteja218@Achiever-83.local>
2018-03-21 19:37:12 +0530

    first commit

---
+++ TBarGraph/BarChartView.swift
@@ -0,0 +1,21 @@
+//
+//  BarChartView.swift
+//  TBarGraph
+//
+//  Created by Teja on 3/21/18.
+//  Copyright © 2018 Teja. All rights reserved.
+//
+
+import UIKit
+
+class BarChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
